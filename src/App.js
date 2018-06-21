f8bb287704ff3c59852376c1af064e1cd4887fcb
github username <github email address>
2018-06-21 18:05:20 -0400

    change the position of the input and buttons

--- src/App.js
+++ src/App.js
@@ -5,41 +5,43 @@ let randomCharacters = ['a','b','c' ,'d','e','f', 'g','h','o','j', 'k', 'l', 'm'
 'n', 'o', 'p', 'q', 'r', 's', 't','u','v', 'w', 'x', 'y', 
 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', 
 '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '[' , ']', '{', '}', 
-'?', '<', '>', '/', ':', ';', '|' ];
+'?', '<', '>', '/', ':', ';', '|' ]; //create an array of characters to pull from later
 
 
 class App extends Component {
 
   state = {message:`Hello Fuzz! Thanks for letting me share my skills with you! 
                     You can scramble any message you like by putting some text in the 
-                    box and pressing *scramble*. Press *unscramble* to unscramble the text. The letters in red are the letters from your message input.`
+                    box and pressing *scramble*. Press *unscramble* to unscramble the text. 
+                    The letters in red are the letters from your message input.`//this is the stock message
   , messageString:''}
 
   handleInput = (e) => {
-    this.setState({message: e.target.value})
+    this.setState({message: e.target.value})//we hold the value of the input box in state to create a controlled input 
   }
 
   componentDidMount(){
-    this.scramble()
+    this.scramble() //when the component mounts, we scramble the default message 
   }
 
   unscramble = () => {
-    let spans = document.body.querySelectorAll('span, br')
+    let spans = document.body.querySelectorAll('span, br')//when we unscrmable, we select all spans and br elements from the dom 
     let messageString = '';
     let spanObjects = [];
-      for(let i = 0; i < spans.length; i++){
+
+      for(let i = 0; i < spans.length; i++){//look over each element
         
-        if(spans[i].hasAttribute('hidden')){
-          spanObjects.push(<span style={{fontSize: 18, color:'red'}}>{spans[i].innerHTML}</span>)
-          messageString += spans[i].innerHTML
-        }else if(spans[i].tagName === "BR"){
-          spanObjects.push(<br />)
+        if(spans[i].hasAttribute('hidden')){//if the attribute is hidden, that means it contains one of the letters to our message
+          spanObjects.push(<span style={{fontSize: 18, color:'red'}}>{spans[i].innerHTML}</span>)//we push it into our spanObjects array and change the color and size
+          messageString += spans[i].innerHTML//add the letter to the message string 
+        }else if(spans[i].tagName === "BR"){//if one of the elements in spans is a break object
+          spanObjects.push(<br />)//create a new break object 
         }else{
-          spanObjects.push(<span>{spans[i].innerHTML}</span>)
+          spanObjects.push(<span>{spans[i].innerHTML}</span>)//if it's just a normal letter, create a new span for it 
         }   
       }
 
-      this.setState({spans: spanObjects, messageString: messageString})
+      this.setState({spans: spanObjects, messageString: messageString}) //set the objects and the new message in state
     
   }
 
@@ -47,26 +49,26 @@ class App extends Component {
 
   scramble = () => {
 
-    if(this.state.message !== ''){
+    if(this.state.message !== ''){ //checks to make sure there is something to scramble 
         let spans = []; 
-        let messageArray = this.state.message.split('')
+        let messageArray = this.state.message.split('')//we split the input message into an array 
         this.setState({messageString: ""})
         messageArray.forEach(char => {
 
-          spans.push(<span hidden>{char}</span>)
+          spans.push(<span hidden>{char}</span>) //we push each letter of our message into a new spans array 
 
-          let randomIndex = Math.floor(Math.random() * Math.floor(70)) 
-
-          for(let i = 0; i < randomIndex; i++){
-            let randomChar = randomCharacters[Math.floor(Math.random() * Math.floor(randomCharacters.length))]; 
+          let numRandomChars = Math.floor(Math.random() * Math.floor(70)) //we generate a random number of random chars
+          //this is the number of random chars we have inbetween letters from our message
+          for(let i = 0; i < numRandomChars; i++){
+            let randomChar = randomCharacters[Math.floor(Math.random() * Math.floor(randomCharacters.length))]; //select a random char from our array 
             spans.push(<span>{randomChar}</span>)
 
-            if(spans.length % 100 === 0){
+            if(spans.length % 100 === 0){//every 100 lines, break
               spans.push(<br />)
             }
           }
       })
-      this.setState({spans: spans})
+      this.setState({spans: spans})//set the spans in state
 
     }else{
       alert('Please enter a message to scramble')
@@ -77,8 +79,8 @@ class App extends Component {
   
 
     render() {  
-      if(this.state.spans){
-        return (
+      if(this.state.spans){ //if we're finished processing the message,
+        return (//render the controls, the spans, and the decoded message if we have it
             <div style={{margin: '10px'}}>
               <ScramblerControls 
                                 unscramble={this.unscramble}
@@ -98,7 +100,7 @@ class App extends Component {
           </div>
         )
     }else{
-      return(
+      return(//else just render the controls
         <div style={{margin: '10px'}}>
         <ScramblerControls 
                             unscramble={this.unscramble}
--- src/ScramblerControls.js
+++ src/ScramblerControls.js
@@ -5,9 +5,9 @@ class ScramblerControls extends React.Component{
     render(){
         return(
             <div>
+                <input style={{width: 300, height: 15}} onChange={this.props.handleInput} value={this.props.message} />
                 <button onClick={this.props.scramble}>Scramble</button> 
                 <button onClick={this.props.unscramble}>Unscramble</button>
-                <input onChange={this.props.handleInput} value={this.props.message} />
             </div>
 
         )
